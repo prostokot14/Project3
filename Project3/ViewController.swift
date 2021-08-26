42eefed2debe772154b6daa6c578dc95089abc84
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2021-08-26 14:17:25 +0300

    Initial Commit

---
+++ Project3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project3
+//
+//  Created by Антон Кашников on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
