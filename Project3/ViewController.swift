954f506198e62e2355af27fcf97069ce0e51442e
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2021-08-26 14:19:18 +0300

    Delete empty strings

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -4,16 +4,10 @@
 //
 //  Created by Антон Кашников on 26.08.2021.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
