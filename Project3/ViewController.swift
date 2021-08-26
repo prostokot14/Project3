2ecb6abc4b0c7c87cf283df743cc179848a48a1e
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2021-08-26 14:22:08 +0300

    Change main ViewController class so that builds on UITableViewController instead

--- Project3/ViewController.swift
+++ Project3/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Антон Кашников on 26.08.2021.
 //
 import UIKit
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
